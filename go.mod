6b0930783788d5f5e0c1257134a8ec90fe72d1ef
Phil <phil.dev@mailbox.org>
2021-05-07 19:56:00 +0200

    updated models

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/philohsophy/dummy-blockchain-models
 
 go 1.16
+
+require github.com/google/uuid v1.2.0
--- structs.go
+++ structs.go
@@ -1,14 +1,42 @@
 package models
 
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+
+	"github.com/google/uuid"
+)
+
 type Address struct {
 	Name        string `json:"name"`
 	Street      string `json:"street"`
-	HouseNumber int    `json:"houseNumber"`
+	HouseNumber string `json:"houseNumber"`
 	Town        string `json:"town"`
 }
 
+// Make the Address struct implement the driver.Valuer interface. This method
+// simply returns the JSON-encoded representation of the struct.
+// Ref: https://www.alexedwards.net/blog/using-postgresql-jsonb
+func (a Address) Value() (driver.Value, error) {
+	return json.Marshal(a)
+}
+
+// Make the Address struct implement the sql.Scanner interface. This method
+// simply decodes a JSON-encoded value into the struct fields.
+// Ref: https://www.alexedwards.net/blog/using-postgresql-jsonb
+func (a *Address) Scan(value interface{}) error {
+	b, ok := value.([]byte)
+	if !ok {
+		return errors.New("type assertion to []byte failed")
+	}
+
+	return json.Unmarshal(b, &a)
+}
+
 type Transaction struct {
-	RecipentAddress Address `json:"recipientAddress"`
-	SenderAddress   Address `json:"senderAddress"`
-	Value           string  `json:"value"`
+	Id               uuid.UUID `json:"id"`
+	RecipientAddress Address   `json:"recipientAddress"`
+	SenderAddress    Address   `json:"senderAddress"`
+	Value            float32   `json:"value"`
 }
