82e4d7478bebe3e3ab423b06015d02b148017a8a
Phil <phil.dev@mailbox.org>
2021-05-08 17:44:07 +0200

    Implemented and tested IsValid() checks

--- models.go
+++ models.go
@@ -34,9 +34,19 @@ func (a *Address) Scan(value interface{}) error {
 	return json.Unmarshal(b, &a)
 }
 
+func (a *Address) IsValid() bool {
+	return a.Name != "" && a.Street != "" && a.HouseNumber != "" && a.Town != ""
+}
+
 type Transaction struct {
 	Id               uuid.UUID `json:"id"`
 	RecipientAddress Address   `json:"recipientAddress"`
 	SenderAddress    Address   `json:"senderAddress"`
 	Value            float32   `json:"value"`
+}
+
+// Alterantive: use factory function and make structs private
+// http://www.golangpatterns.info/object-oriented/constructors
+func (t *Transaction) IsValid() bool {
+	return t.Id != uuid.Nil && t.SenderAddress.IsValid() && t.RecipientAddress.IsValid() && t.Value > 0
 }
---
+++ models_test.go
@@ -0,0 +1,105 @@
+package models_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/google/uuid"
+	models "github.com/philohsophy/dummy-blockchain-models"
+)
+
+func TestMain(m *testing.M) {
+	code := m.Run()
+	os.Exit(code)
+}
+
+func createValidTransaction() models.Transaction {
+	recipientAddress := models.Address{Name: "Foo", Street: "FooStreet", HouseNumber: "1", Town: "FooTown"}
+	senderAddress := models.Address{Name: "Bar", Street: "BarStreet", HouseNumber: "1", Town: "BarTown"}
+
+	var t models.Transaction
+	t.Id = uuid.New()
+	t.RecipientAddress = recipientAddress
+	t.SenderAddress = senderAddress
+	t.Value = 100.21
+
+	return t
+}
+
+func TestValidTransaction(t *testing.T) {
+	transaction := createValidTransaction()
+	if !transaction.IsValid() {
+		t.Error("Expected transaction to be valid")
+	}
+}
+
+func TestInvalidTransaction(t *testing.T) {
+	transaction := createValidTransaction()
+
+	t.Run("Check Id", func(t *testing.T) {
+		invalidTransaction := models.Transaction{SenderAddress: transaction.SenderAddress, RecipientAddress: transaction.RecipientAddress, Value: transaction.Value}
+
+		if invalidTransaction.IsValid() {
+			t.Error("Expected transaction to be invalid (Id is 'nil')")
+		}
+	})
+
+	t.Run("Check SenderAddress", func(t *testing.T) {
+		transaction := models.Transaction{Id: transaction.Id, RecipientAddress: transaction.RecipientAddress, Value: transaction.Value}
+
+		var invalidTransactions = make(map[string]models.Transaction)
+		invalidTransactions["nil"] = transaction
+		transaction.SenderAddress = models.Address{Street: "FooStreet", HouseNumber: "1", Town: "FooTown"}
+		invalidTransactions["missing 'Name'"] = transaction
+		transaction.SenderAddress = models.Address{Name: "Foo", HouseNumber: "1", Town: "FooTown"}
+		invalidTransactions["missing 'Street'"] = transaction
+		transaction.SenderAddress = models.Address{Name: "Foo", Street: "FooStreet", Town: "FooTown"}
+		invalidTransactions["missing 'HouseNumber'"] = transaction
+		transaction.SenderAddress = models.Address{Name: "Foo", Street: "FooStreet", HouseNumber: "1"}
+		invalidTransactions["missing 'Town'"] = transaction
+
+		for key, invalidTransaction := range invalidTransactions {
+			if invalidTransaction.IsValid() {
+				t.Errorf("Expected transaction to be invalid (RecipientAddress is %s)", key)
+			}
+		}
+	})
+
+	t.Run("Check RecipientAddress", func(t *testing.T) {
+		transaction := models.Transaction{Id: transaction.Id, SenderAddress: transaction.SenderAddress, Value: transaction.Value}
+
+		var invalidTransactions = make(map[string]models.Transaction)
+		invalidTransactions["nil"] = transaction
+		transaction.RecipientAddress = models.Address{Street: "FooStreet", HouseNumber: "1", Town: "FooTown"}
+		invalidTransactions["missing 'Name'"] = transaction
+		transaction.RecipientAddress = models.Address{Name: "Foo", HouseNumber: "1", Town: "FooTown"}
+		invalidTransactions["missing 'Street'"] = transaction
+		transaction.RecipientAddress = models.Address{Name: "Foo", Street: "FooStreet", Town: "FooTown"}
+		invalidTransactions["missing 'HouseNumber'"] = transaction
+		transaction.RecipientAddress = models.Address{Name: "Foo", Street: "FooStreet", HouseNumber: "1"}
+		invalidTransactions["missing 'Town'"] = transaction
+
+		for key, invalidTransaction := range invalidTransactions {
+			if invalidTransaction.IsValid() {
+				t.Errorf("Expected transaction to be invalid (RecipientAddress is %s)", key)
+			}
+		}
+	})
+
+	t.Run("Check Value", func(t *testing.T) {
+		transaction := models.Transaction{Id: transaction.Id, SenderAddress: transaction.SenderAddress, RecipientAddress: transaction.RecipientAddress}
+
+		var invalidTransactions = make(map[string]models.Transaction)
+		invalidTransactions["nil"] = transaction
+		transaction.Value = 0.00
+		invalidTransactions["null"] = transaction
+		transaction.Value = -100.21
+		invalidTransactions["negative"] = transaction
+
+		for key, invalidTransaction := range invalidTransactions {
+			if invalidTransaction.IsValid() {
+				t.Errorf("Expected transaction to be invalid (Value is '%s')", key)
+			}
+		}
+	})
+}
